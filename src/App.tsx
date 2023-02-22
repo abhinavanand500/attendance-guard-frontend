7071c11b6c4c5be7ac59238074b5938036c3f442
abhinavanand500 <abhinav.anand500@gmail.com>
2023-02-23 01:25:58 +0530

    Navbar and Routing Completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,26 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./screen/Home";
+import Navbar from "./components/Navbar";
+import Feed from "./screen/Feed";
+import Team from "./screen/Team";
+import Guard from "./screen/Guard";
+import Contact from "./screen/Contact";
 
 function App() {
-  return (
-    <div>
-      <h1 className="text-3xl font-bold">
-      Hello
-      </h1>
-    </div>
-  );
+	return (
+		<BrowserRouter>
+			<Navbar />
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/feed" element={<Feed />} />
+				<Route path="/team" element={<Team />} />
+				<Route path="/guard" element={<Guard />} />
+				<Route path="/contact" element={<Contact />} />
+			</Routes>
+		</BrowserRouter>
+	);
 }
 
 export default App;
